1fc87f62afa8f9598d79ae08605d212a42b6c4c3
Cheryl Miller <cmiller112000@gmail.com>
2017-03-23 18:37:50 -0500

    part 2 complete

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -68,6 +68,22 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
+    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+                 min_n_components=2, max_n_components=10,
+                 random_state=14, verbose=False):
+        super(SelectorBIC,self).__init__(all_word_sequences,all_word_Xlengths,this_word,3,min_n_components,max_n_components,random_state,verbose)
+        self.model_cands = dict()
+        for n in range(self.min_n_components,self.max_n_components):
+            try:
+                model = self.base_model(n)
+                N=math.fsum(self.lengths)
+                p = (n * (n-1)) + (n * (model.n_features-1))
+                L =model.score(self.X,self.lengths)
+                thisscore= (-2.0 * L) + (p * math.log(N))
+                self.model_cands[thisscore] = model
+            except:
+                pass
+
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
@@ -77,6 +93,10 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
+        if len(self.model_cands) == 0:
+            return self.base_model(self.min_n_components)
+        minscore=min(self.model_cands.keys())
+        return self.model_cands[minscore]
         raise NotImplementedError
 
 
@@ -89,10 +109,46 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
+    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+                 min_n_components=2, max_n_components=10,
+                 random_state=14, verbose=False):
+        super(SelectorDIC,self).__init__(all_word_sequences,all_word_Xlengths,this_word,3,min_n_components,max_n_components,random_state,verbose)
+        self.model_cands = dict()
+        for n in range(self.min_n_components,self.max_n_components):
+            try:
+                model = self.base_model(n)
+                myscore =model.score(self.X,self.lengths)
+                othertotalscore = 0.0
+                othermodelcount = 0.0
+                otherwords = self.words.keys()
+                othermodelcount = len(otherwords) - 1
+                for w in otherwords:
+                    if w == this_word:
+                        continue
+                    try:
+                        ms = ModelSelector(self.words, self.hwords, w, 3, self.min_n_components, self.max_n_components, self.random_state, self.verbose )
+                        othermodel = ms.base_model(n)
+                        try:
+                            otherscore = othermodel.score(ms.X, ms.lengths)
+                            othertotalscore += otherscore
+                        except:
+                            pass
+                    except:
+                        pass
+                avgotherscore = othertotalscore/othermodelcount
+                thisscore = myscore - avgotherscore
+                self.model_cands[thisscore] = model
+            except:
+                pass
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on DIC scores
+        if len(self.model_cands) == 0:
+            return self.base_model(self.min_n_components)
+        maxscore=max(self.model_cands.keys())
+        return self.model_cands[maxscore]
         raise NotImplementedError
 
 
@@ -100,9 +156,39 @@ class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
+    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+                 min_n_components=2, max_n_components=10,
+                 random_state=14, verbose=False):
+        super(SelectorCV,self).__init__(all_word_sequences,all_word_Xlengths,this_word,3,min_n_components,max_n_components,random_state,verbose)
+        self.model_cands = dict()
+        for n in range(self.min_n_components,self.max_n_components):
+            scoretotal=0.0
+            avgscore = 0.0
+            numscores=0
+            folds=min(len(self.sequences),3)
+            split_method = KFold(folds)
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                word_sequences, word_Xlengths = combine_sequences(cv_train_idx,self.sequences)
+
+                try:
+                    model = self.base_model(n)
+                    thisscore=model.score(word_sequences,word_Xlengths)
+                    scoretotal += thisscore
+                    numscores += 1
+                except:
+                    pass
+                    model = None
+            if numscores > 0:
+                avgscore = scoretotal/numscores
+            if model is not None:
+                self.model_cands[avgscore] = model
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
+        if len(self.model_cands) == 0:
+            return self.base_model(self.min_n_components)
+        maxscore=max(self.model_cands.keys())
+        return self.model_cands[maxscore]
         raise NotImplementedError
